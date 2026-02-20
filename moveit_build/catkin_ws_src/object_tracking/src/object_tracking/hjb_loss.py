93bb4f55ce5c101a33c8a529bf8852005d5d7df5
shadadu <sxantwi@gmail.com>
2026-02-20 13:45:18 -0500

    Update hjb_loss.py

--- moveit_build/catkin_ws_src/object_tracking/src/object_tracking/hjb_loss.py
+++ moveit_build/catkin_ws_src/object_tracking/src/object_tracking/hjb_loss.py
@@ -13,10 +13,10 @@ def hjb_residual_loss(V, q, t_norm, running_cost, R_inv_diag):
     """
     grad_q = torch.autograd.grad(V.sum(), q, create_graph=True, retain_graph=True)[0]  # (B,7)
     V_t = torch.autograd.grad(V.sum(), t_norm, create_graph=True, retain_graph=True)[0].squeeze(-1)  # (B,)
-    print(f'Shapes of tensors, value of R_inv_diag: {grad_q.shape}, {grad_q.T.shape}, {R_inv_diag.shape}, {R_inv_diag}')
+    # print(f'Shapes of tensors, value of R_inv_diag: {grad_q.shape}, {grad_q.T.shape}, {R_inv_diag.shape}, {R_inv_diag}')
     # quad = (grad_q.T * R_inv_diag * grad_q).sum(dim=-1)  # (B,)
     quad = torch.matmul(grad_q, grad_q.T).sum(dim=-1)  # (B,)
-    print(f'shape of quad: {torch.matmul(grad_q.T, grad_q).shape}, {quad.shape}')
+    # print(f'shape of quad: {torch.matmul(grad_q.T, grad_q).shape}, {quad.shape}')
     resid = V_t + running_cost - 0.25 * quad
     return (resid ** 2).mean()
 
