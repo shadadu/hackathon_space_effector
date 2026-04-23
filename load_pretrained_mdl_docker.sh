874f6d6dbca97b373a3c0cd35548bef726316a5f
shadadu <sxantwi@gmail.com>
2026-04-23 10:44:52 -0400

    Create load_pretrained_mdl_docker.sh

---
+++ load_pretrained_mdl_docker.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+NET_NAME="${NET_NAME:-rosnet}"
+ROS_MASTER_NAME="${ROS_MASTER_NAME:-ros_master}"
+ASTROBEE_NAME="${ASTROBEE_NAME:-astrobee}"
+MOVEIT_NAME="${MOVEIT_NAME:-moveit}"
+
+
+
+
+MOVEIT_STATE=`docker inspect -f '{{.State.Running}}' moveit`
+
+if [ "$MOVEIT_STATE" == "true" ]; then
+    echo $MOVEIT_STATE "$MOVEIT_NAME" "container is running"
+    RESULT_MODEL="${RESULT_DIR:-/Users/rckyi/Documents/results/panda_dgm_v1.pth}"
+     docker cp "$RESULT_MODEL" "$MOVEIT_NAME":'$model_path'
+else
+    echo $MOVEIT_STATE "$MOVEIT_NAME" "container is NOT running"
+fi
+
+
+
+
+log() { echo -e "\n\033[1;34m[INFO]\033[0m $*"; }
+ok()  { echo -e "\033[1;32m[PASS]\033[0m $*"; }
+
+
+
+
